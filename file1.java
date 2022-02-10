65673c031553577fd59ac0c4611f5152e2c11637
Constança Rigueira <83832283+conyrig@users.noreply.github.com>
2022-02-10 23:46:47 +0000

    Add files via upload

---
+++ file1.java
@@ -0,0 +1,25 @@
+public class file1 {
+    /*
+     * public static void main(String[] args) {
+     * // addExclamationPoint("Have fun");
+     * String exclaim = addExclamationPoint("Have fun");
+     * System.out.println(exclaim);
+     * }
+     * 
+     * // Option 1
+     * // public static void addExclamationPoint(String s) {
+     * // System.out.println(s + "!");
+     * // }
+     * 
+     * // Option 2
+     * public static String addExclamationPoint(String s) {
+     * return s + "!";
+     * }
+     */
+
+    public static void main(String[] args) {
+        Animal a = new Animal();
+        String dog = a.iAmDog();
+        System.out.println(dog);
+    }
+}
